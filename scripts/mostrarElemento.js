76738b64366afc65519f34ceca49709655aab5ca
sr.aether.13@gmail.com <aether@Artemis.localdomain>
2024-05-04 17:05:59 -0600

    Sección de contenido lista, pie de pagina listo, dudas listo

--- scripts/mostrarElemento.js
+++ scripts/mostrarElemento.js
@@ -1,8 +1,15 @@
 function mostrarElemento(idElemento) {
-    // mostramos el documento
-    document.getElementById(idElemento).style.display = "block";
-    // lo ocultamos en 10 segundos
+    document.getElementById(idElemento).style.opacity = 0; // Opacidad 0
+    document.getElementById(idElemento).style.display = "flex"; // O block
     setTimeout(function() {
-        document.getElementById(idElemento).style.display = "none";
-    }, 10000);
-}
+      document.getElementById(idElemento).style.opacity = 1; // Opacidad 1
+    }, 100); // Ajusta el tiempo de espera (en milisegundos)
+    // ocultamos el elemento a los 7 segundos
+    setTimeout(function() {
+      document.getElementById(idElemento).style.opacity = 0; // Opacidad 0
+      setTimeout(function() {
+        document.getElementById(idElemento).style.display = "none"; // O block
+      }, 1000); // Ajusta el tiempo de espera (en milisegundos)
+    }, 7000); // Ajusta el tiempo de espera (en milisegundos)
+  }
+  
